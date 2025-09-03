b3cd069beacc00c8ac3a55e4f418070d33cb9ab5
priz80 <priz8090@gmail.com>
2025-09-04 01:04:17 +0300

    Create menu-burger.js

---
+++ menu-burger.js
@@ -0,0 +1,4 @@
+document.querySelector(".burger").addEventListener("click", () => {
+  document.querySelector(".open-menu-burger").classList.toggle("menu-burger")
+  document.querySelector(".open-menu").classList.toggle("m-menu");
+});
