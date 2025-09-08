8a6733518eb6413f7c6aa36bb9ff5074dd8ebd19
priz80 <priz8090@gmail.com>
2025-09-09 01:42:11 +0300

    Update slide-page.js

--- slide-page.js
+++ slide-page.js
@@ -1,20 +1,18 @@
 const page1 = document.querySelector(".page-slide");
 const page2 = document.querySelector(".page-main");
 
-page1.style.display = "none"; // Скрываем первую страницу
+page1.style.transform = `translateX(-100%)`; // Скрываем первую страницу
 
 document.getElementById("header-btn").addEventListener("click", () => {
   /* function goToPage2() { */
   /* const page1 = document.querySelector(".page-slide");
   const page2 = document.querySelector(".main-page"); */
-
-  page1.style.display = "block"; // Скрываем первую страницу
-  page2.style.display = "none"; // Показываем вторую страницу
+  page2.style.transform = `translateX(-100%)`;
+  page1.style.transform = `translateX(100%)`;
+  /* page1.style.display = "block"; // Скрываем первую страницу
+  page2.style.display = "none"; // Показываем вторую страницу */
   /* } */
 });
 
 
 
-document.getElementById("refreshBtn").addEventListener("click", () => {
-    location.reload(); // Перезагружает страницу
-});
