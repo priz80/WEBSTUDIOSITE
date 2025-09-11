e7d9ceae76eb6b9b8c5a4a352a13a0b9caf7726c
priz80 <priz8090@gmail.com>
2025-09-11 15:25:13 +0300

    Update send-email.php

--- send-email.php
+++ send-email.php
@@ -19,22 +19,57 @@ $email = trim($email);
 $tel = trim($tel);
 $sum = trim($sum);
 $sum = trim($comment);
-//echo $fio;
-//echo "<br>";
-//echo $email;
-//echo "<br>";
-//echo $tel;
-//echo "<br>";
-//echo $sum;
-//echo "<br>";
-//echo $comment;
-//echo "<br>";
+echo $fio;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $tel;
+echo "<br>";
+echo $sum;
+echo "<br>";
+echo $comment;
+echo "<br>";
 
 if (mail("priz8090@gmail.com", "Заявка с сайта", "ФИО: ".$fio."\n\nE-mail: ".$email."\n\nТелефон: ".$tel."\n\nБюджет: ".$sum."\n\nСообщение: ".$comment ,"From: admin@pagelist.ru \r\n")){     
     echo "сообщение успешно отправлено";
-    header("Refresh:5; url=https://pagelist.ru/");
+    header("Refresh:5; url=http://127.0.0.1:5500/");
 } 
 else {
     echo "при отправке сообщения возникли ошибки";
-}?>
+}
+
+
+require_once '/PHPMailer/PHPMailerAutoload.php';
+ 
+$mail = new PHPMailer;
+$mail->CharSet = 'UTF-8';
+ 
+// Настройки SMTP
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+$mail->SMTPDebug = 0;
+ 
+$mail->Host = 'ssl://smtp.gmail.com';
+$mail->Port = 465;
+$mail->Username = 'priz8090@gmail.com';
+$mail->Password = 'L5opc75555';
+ 
+// От кого
+$mail->setFrom('mail@ws.ru', 'ws.ru');        
+ 
+// Кому
+$mail->addAddress('priz8090@gmail.com', 'Иван Петров');
+ 
+// Тема письма
+$mail->Subject = $subject;
+ 
+// Тело письма
+$body = '<p><strong>«Hello, world!» </strong></p>';
+$mail->msgHTML($body);
+ 
+// Приложение
+$mail->addAttachment(__DIR__ . '/image.jpg');
+ 
+$mail->send();
+?>
 
